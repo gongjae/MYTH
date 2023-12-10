10bdcfe92aae79bf6c4016aa476f322a281a0f55
gongjae <rudddong@gmail.com>
2023-12-10 18:49:38 +0900

    final-4

--- src/app/signIn/page.jsx
+++ src/app/signIn/page.jsx
@@ -2,7 +2,6 @@ import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
 import { authOptions } from '../api/auth/[...nextauth]/route'
 import SignInBtn from '@/components/SignInBtn'
-
 export default async function SignIn() {
   const session = await getServerSession(authOptions)
   if (session) {
@@ -10,7 +9,7 @@ export default async function SignIn() {
   }
   return (
     <div className="mt-16">
-      <SignInBtn /> 
+      <SignInBtn />
     </div>
   )
 }
