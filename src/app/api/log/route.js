ec02a68c57677766e278af26d1963502c7d42e5e
gongjae <rudddong@gmail.com>
2023-12-10 21:00:51 +0900

    final-5

--- src/app/api/log/route.js
+++ src/app/api/log/route.js
@@ -1,8 +1,9 @@
 import connectMongoDB from '@/libs/mongodb'
 import Log from '@/models/log'
 import { NextResponse } from 'next/server'
+
 export async function POST(request) {
-  const { email } = await request.json()
+  const { name, email, provider } = await request.json()
   await connectMongoDB()
   await Log.create({ email })
   return NextResponse.json({ message: 'Login event logged' }, { status: 201 })
