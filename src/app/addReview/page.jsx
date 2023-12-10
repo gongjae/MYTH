ebf376e395a1406bea5ba48208984402a421e16b
gongjae <rudddong@gmail.com>
2023-12-10 23:03:55 +0900

    final-5

--- src/app/addReview/page.jsx
+++ src/app/addReview/page.jsx
@@ -11,23 +11,23 @@ export default function AddReview() {
     e.preventDefault()
     if (!title || !description) {
       alert('리뷰를 등록해주세요')
+      return // 리뷰가 비어있는 경우 바로 종료
     }
     try {
       const res = await fetch(`${apiUrl}/api/reviews`, {
         method: 'POST',
         headers: {
-          'Content-type': 'application/json',
+          'Content-Type': 'application/json', // 오타 수정
         },
         body: JSON.stringify({ title, description }),
       })
       if (res.ok) {
-        router.push('/')
-        router.refresh()
+        router.reload() // 또는 router.push('/')를 사용할 수 있습니다.
       } else {
         throw new Error('리뷰를 등록하는 데 실패하였습니다')
       }
     } catch (error) {
-      console.log(error)
+      console.error(error)
     }
   }
   return (
@@ -55,38 +55,3 @@ export default function AddReview() {
     </form>
   )
 }
-
-// import AddReviewForm from '@/components/AddReviewForm'
-// import { getServerSession } from 'next-auth'
-// import { authOptions } from '../api/auth/[...nextauth]/route'
-// import { redirect } from 'next/navigation'
-
-// export default async function AddReviewPage() {
-//   const session = await getServerSession(authOptions)
-
-//   if (!session) {
-//     redirect('/signIn')
-//   }
-
-//   return <AddReviewForm />
-// }
-//위에꺼 아래꺼 따로임
-// export default function AddReview() {
-//   return (
-//     <form className="flex flex-col gap-3">
-//       <input
-//         className="border border-slate-500 p-4"
-//         type="text"
-//         placeholder="Review Title"
-//       />
-//       <textarea
-//         className="border border-slate-500 p-4 h-32"
-//         type="text"
-//         placeholder="Review Description"
-//       ></textarea>
-//       <button className="bg-blue-800 text-white font-bold px-6 py-3 w-fit rounded-md">
-//         리뷰 추가하기
-//       </button>
-//     </form>
-//   )
-// }
