07cee467cd5c06d4473c8d6c5922f5423debbd1c
gongjae <rudddong@gmail.com>
2023-12-10 18:40:28 +0900

    final-4

--- src/app/result/page.jsx
+++ src/app/result/page.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import Link from 'next/link'
 import CheckoutWizard from '@/components/CheckoutWizard'
 
 export default function Home() {
--- src/app/signIn/page.jsx
+++ src/app/signIn/page.jsx
@@ -2,6 +2,7 @@ import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
 import { authOptions } from '../api/auth/[...nextauth]/route'
 import SignInBtn from '@/components/SignInBtn'
+
 export default async function SignIn() {
   const session = await getServerSession(authOptions)
   if (session) {
