1e4d2bd7737e9cf456535bafd4f3978c2472e35d
gongjae <rudddong@gmail.com>
2023-12-10 18:52:47 +0900

    final-4

--- src/app/api/log/route.js
+++ src/app/api/log/route.js
@@ -1,7 +1,6 @@
 import connectMongoDB from '@/libs/mongodb'
 import Log from '@/models/log'
 import { NextResponse } from 'next/server'
-
 export async function POST(request) {
   const { email } = await request.json()
   await connectMongoDB()
